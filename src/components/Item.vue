edfb271bfade3832a10709cd4560b6ad1a16e447
Shlomo Badusa <shlomob@tikalk.com>
2018-11-27 11:08:32 +0200

    add gallery

---
+++ src/components/Item.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="item">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Item extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.item {
+  border: solid 1px blue;
+  margin: 5px;
+}
+</style>
---
+++ src/views/Gallery.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="gallery">
+    <Item v-for="item in items" :key="item" :msg="item"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Item from "@/components/Item.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    Item
+  }
+})
+export default class Gallery extends Vue {
+  items: string[] = ["test1", "test2", "test3", "test4"];
+}
+</script>
+
+<style scoped lang="scss">
+.gallery {
+  display: flex;
+  border: solid 1px red;
+}
+</style>
